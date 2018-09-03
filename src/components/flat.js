c0455452b307f21b39f2a7340065721370c8fb13
edthomas93 <et1270@my.bristol.ac.uk>
2018-09-03 16:13:23 +0100

    Renders flat on the app page

--- src/components/flat.js
+++ src/components/flat.js
@@ -1,24 +1,17 @@
 import React from "react";
 import "./flat.css";
 
-// const flat = {
-//   "id": 145,
-//   "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
-//   "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-//   "price": 164,
-//   "priceCurrency": "EUR",
-//   "lat": 48.884211,
-//   "lng": 2.346890
-// }
-// <Flat flat={flat} />
-
 class Flat extends React.Component {
   render() {
-    const title = this.props.flat.price + this.props.flat.priceCurrency + " - " + this.props.flat.name;
+    const title = this.props.flat.price + " " + this.props.flat.priceCurrency + " - " + this.props.flat.name;
+
+    const style = {
+      backgroundImage: `url('${this.props.flat.imageUrl}')`
+    };
 
     return (
       <div className="flat">
-        <div className="flat-picture"></div>
+        <div className="flat-picture" style={style}></div>
         <div className="flat-title"></div>
           {title}
       </div>
