6c6e83ff94472496e63a86aeb03645112a1c63f7
edthomas93 <et1270@my.bristol.ac.uk>
2018-09-03 15:42:31 +0100

    Added title and details to flat.js

--- src/components/flat.js
+++ src/components/flat.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./flat.css";
 
+// const flat = {
+//   "id": 145,
+//   "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
+//   "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+//   "price": 164,
+//   "priceCurrency": "EUR",
+//   "lat": 48.884211,
+//   "lng": 2.346890
+// }
+// <Flat flat={flat} />
+
 class Flat extends React.Component {
   render() {
+    const title = this.props.flat.price + this.props.flat.priceCurrency + " - " + this.props.flat.name;
+
     return (
       <div className="flat">
         <div className="flat-picture"></div>
         <div className="flat-title"></div>
+          {title}
       </div>
     );
   }
