64fcd079b52a55abdcf2249e73cbd9073904c3b7
edthomas93 <et1270@my.bristol.ac.uk>
2018-09-03 15:12:29 +0100

    Created flat component

---
+++ src/components/flat.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Flat extends React.Component {
+  render() {
+    return (
+      <div>
+        <div class="flat"></div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default Flat;
