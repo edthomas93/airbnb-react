af25277c1b462ce3ef229050e24172aa2ecfdbcb
edthomas93 <et1270@my.bristol.ac.uk>
2018-09-04 11:29:51 +0100

    Added map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Flat from './components/flat';
+import GoogleMapReact from 'google-map-react';
 
 class App extends Component {
   constructor(props) {
@@ -22,6 +23,10 @@ class App extends Component {
   }
 
   render() {
+    const center = {
+      lat: 48.8566,
+      lng: 2.3522
+    }
     return (
       <div className="app">
         <div className="main">
@@ -34,6 +39,11 @@ class App extends Component {
           </div>
         </div>
         <div className="map">
+          <GoogleMapReact
+            center={center}
+            zoom={11}
+          >
+          </GoogleMapReact>
         </div>
       </div>
     );
