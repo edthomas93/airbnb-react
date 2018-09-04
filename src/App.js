438555e6d3412155b5e037cccca1814332be0ee2
edthomas93 <et1270@my.bristol.ac.uk>
2018-09-04 11:02:03 +0100

    Get info about flats from json file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Flat from './components/flat';
 
 class App extends Component {
-  render() {
-    const flat = {
-      "id": 145,
-      "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
-      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-      "price": 164,
-      "priceCurrency": "EUR",
-      "lat": 48.884211,
-      "lng": 2.346890
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
     };
+  }
 
+  componentDidMount(){
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url) //AJAX
+      .then(response => response.json()) //converts the raw string from the request into json
+      .then((data) => {
+        console.log(data);
+      })
+  }
+
+  render() {
     return (
-      <div>
-        <Flat flat={flat} />.
-        <Flat flat={flat} />
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          </div>
+          <div className="flats">
+            {this.state.flats.map(function(flat) {
+              return <Flat flat={flat} />
+            })}
+          </div>
+        </div>
+        <div className="map">
+        </div>
       </div>
     );
   }
